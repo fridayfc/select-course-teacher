2cae9dd325d819f714c8f7e8cdfb0652c6181e45
fridayfc <47932488+fridayfc@users.noreply.github.com>
2019-03-20 16:48:52 +0800

    Update TestController.java
    test changes for IDEA

--- src/main/java/edu/snail/controller/TestController.java
+++ src/main/java/edu/snail/controller/TestController.java
@@ -1,7 +1,6 @@
 package edu.snail.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import edu.snail.model.SysUser;
 import edu.snail.service.TestService;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
